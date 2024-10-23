c86fcae07b4e40bfcbc93cb9bbb5068e4d197930
Alejandro Ochoa <alex.jym13@gmail.com>
2024-10-22 23:50:48 -0500

    Added core libraries to core module also create some setup for DI

---
+++ app/src/main/java/com/aor/bank/Application.kt
@@ -0,0 +1,7 @@
+package com.aor.bank
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationBank : Application()
---
+++ core/src/main/java/com/aor/bank/core/data/navigation/NavigationRoute.kt
@@ -0,0 +1,10 @@
+package com.aor.bank.core.data.navigation
+
+sealed class NavigationRoute(val route: String) {
+    data object Home : NavigationRoute("home")
+    data object SignIn : NavigationRoute("sign_in")
+    data object SignUp : NavigationRoute("sign_up")
+    data object TransactionDetail : NavigationRoute("transaction_detail/{transactionId}") {
+        fun createRoute(transactionId: String) = "transaction_detail/$transactionId"
+    }
+}
